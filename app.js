8b02d5ee8eb6694552d18f0edcb4b3a240aa0685
HaninIsmail01 <haninmonir01@gmail.com>
2022-12-02 00:03:46 +0200

    files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 dotenv.config({
-  path: ' ./config/.env'
+  path: './config/.env'
 });
 
 const PORT = process.env.PORT;
@@ -10,6 +10,6 @@ const PORT = process.env.PORT;
 const app = express();
 
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => { 
   console.log(`listening to port number ${PORT}`);
 });
