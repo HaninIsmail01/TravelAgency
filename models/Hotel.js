498b936e66d230d15a38b93adac35bd62d673202
HaninIsmail01 <haninmonir01@gmail.com>
2022-12-03 16:12:20 +0200

    ..

--- models/Hotel.js
+++ models/Hotel.js
@@ -2,6 +2,11 @@ const {Schema, model}= require('mongoose');
 
 const UserSchema= new Schema({
 
+    HotelId: {
+        type: 'intger',
+        required: true
+    },
+    
     
     Hoteladdress: {
         type: 'string',
