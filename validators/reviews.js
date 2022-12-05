1acc77edcdae404b2fbdd1f0f2090e8746a71fcc
Mohammed Adel <93832294+mu7amed-adell@users.noreply.github.com>
2022-12-05 14:17:18 +0200

    v3.3
    ana mesh 3aref da eh :)

---
+++ validators/reviews.js
@@ -0,0 +1,13 @@
+const {check} = require("express-validator");
+
+module.exports.validatePostReview = () => {
+    const ValidationMiddleWare = [
+
+        check('ReviewDescription').notEmpty().withMessage('Please include your review description.'),
+        check("ReviewRating").notEmpty().withMessage('Please include your review rating.'),
+        check('ReviewDate').notEmpty().withMessage('Please include the date of your review.')
+
+    ];
+    return ValidationMiddleWare;
+};
+// file is not yet connected with the Router or Controller
