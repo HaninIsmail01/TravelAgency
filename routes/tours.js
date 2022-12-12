70e736ddd759480123609695894e03123ff3c20b
Mohammed Adel <93832294+mu7amed-adell@users.noreply.github.com>
2022-12-12 13:25:55 +0200

    Update tours.js

--- routes/tours.js
+++ routes/tours.js
@@ -1,10 +1,11 @@
 const{Router} = require('express');
 
 const TourController = require('../controllers/tours');
+
 const TourRouter = Router();
 
-ReviewsRouter.get('/', TourController.getTours);
-ReviewsRouter.post('/', TourController.postTour);
-bookingsRouter.delete('/:tourID', TourController.deleteTour);
+TourRouter.get('/', TourController.getTours);
+TourRouter.post('/', TourController.postTour);
+TourRouter.delete('/:tourID', TourController.deleteTour);
 
 module.exports = ToursRouter;
